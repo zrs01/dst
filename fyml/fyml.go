4878eaf802c0b44d4dd4c1367f208218a3f6b3d4
Rae Wong <rae.wong@gmail.com>
2024-01-10 17:32:47 +0800

    proof of concept

--- fyml/fyml.go
+++ fyml/fyml.go
@@ -1,38 +1,40 @@
-package xfmr
+package fyml
 
 import (
 	"fmt"
-	"strings"
-
 	"io/fs"
 	"os"
+	"strings"
 
-	"github.com/rotisserie/eris"
-	"github.com/sirupsen/logrus"
-	"gopkg.in/yaml.v2"
+	"github.com/ztrue/tracerr"
+	"gopkg.in/yaml.v3"
 )
 
-type OutColumn struct {
-	Values Column `yaml:"_column_values,flow,omitempty"`
-}
-
-func (s *Xfmr) LoadYaml(infile string) {
-	yamlFile, err := os.ReadFile(infile)
+func ReadYaml(file string) (*DataDef, error) {
+	yamlFile, err := os.ReadFile(file)
 	if err != nil {
-		panic(fmt.Sprintf("failed to read the file %s", infile))
+		return nil, tracerr.Wrap(err)
 	}
-	var d InDB
+
+	var node yaml.Node
+	if err := yaml.Unmarshal(yamlFile, &node); err != nil {
+		return nil, tracerr.Wrap(err)
+	}
+	fmt.Printf("%+v\n", node)
+	bytes, err := yaml.Marshal(&node)
+	fmt.Println(string(bytes))
+
+	var d DataDef
 	if err := yaml.Unmarshal(yamlFile, &d); err != nil {
-		panic(fmt.Sprintf("failed to unmarshal the file %s", infile))
+		return nil, tracerr.Wrap(err)
 	}
-	s.Data = &d
+	return &d, err
 }
 
-func (s *Xfmr) SaveToYaml(outfile string) error {
-	yaml.FutureLineWrap()
-	bytes, err := yaml.Marshal(s.Data)
+func WriteYaml(data *DataDef, outfile string) error {
+	bytes, err := yaml.Marshal(data)
 	if err != nil {
-		return eris.Wrap(err, "failed to transform to yaml")
+		return tracerr.Wrap(err)
 	}
 	output := string(bytes)
 	// correct the names
@@ -42,20 +44,24 @@ func (s *Xfmr) SaveToYaml(outfile string) error {
 	output = strings.ReplaceAll(output, "\"n\"", "n")
 	output = strings.ReplaceAll(output, "\"Y\"", "Y")
 	output = strings.ReplaceAll(output, "\"y\"", "y")
-	os.WriteFile(outfile, []byte(output), fs.FileMode(0744))
+	if outfile == "" {
+		// fmt.Println(output)
+	} else {
+		os.WriteFile(outfile, []byte(output), fs.FileMode(0744))
+	}
 	return nil
 }
 
-func (s *Xfmr) Verify() error {
+func Verify(data *DataDef) {
 	tables := make(map[string][]Column)
 
 	// convert to map for easy searching
-	for _, schema := range s.Data.Schemas {
+	for _, schema := range data.Schemas {
 		for _, table := range schema.Tables {
 			tables[table.Name] = table.Columns
 		}
 	}
-	tables["fixed"] = s.Data.Fixed
+	tables["fixed"] = data.Fixed
 
 	isFKExist := func(fk string) bool {
 		// fk format: table.field
@@ -73,18 +79,17 @@ func (s *Xfmr) Verify() error {
 	for k, columns := range tables {
 		for i, column := range columns {
 			if column.Name == "" {
-				logrus.Warnf("'%s', missing column name at line %d", k, i)
+				fmt.Printf("'%s', missing column name at line %d", k, i)
 			}
 			if column.DataType == "" {
-				logrus.Warnf("'%s', missing data type of the column '%s' at line %d", k, column.Name, i)
+				fmt.Printf("'%s', missing data type of the column '%s' at line %d", k, column.Name, i)
 			}
 			if column.ForeignKey != "" {
 				// check the foreign key whether exists
 				if !isFKExist(column.ForeignKey) {
-					logrus.Warnf("'%s', FK '%s' cannot be found at line %d\n", k, column.ForeignKey, i)
+					fmt.Printf("'%s', FK '%s' cannot be found at line %d\n", k, column.ForeignKey, i)
 				}
 			}
 		}
 	}
-	return nil
 }
--- main.go
+++ main.go
@@ -5,17 +5,23 @@ import (
 	"path/filepath"
 	"strings"
 
-	"github.com/codeskyblue/go-sh"
-	"github.com/rotisserie/eris"
-	"github.com/shomali11/util/xstrings"
-	"github.com/sirupsen/logrus"
-	"github.com/thoas/go-funk"
+	"github.com/samber/lo"
 	"github.com/urfave/cli/v2"
+	"github.com/ztrue/tracerr"
 
-	xmfr "dst/xfmr"
+	"dst/fyml"
 )
 
 var version = "development"
+
+// input   output   options
+// ------------------------
+// yaml    xlsx     simple
+// yaml    <text>   template
+// yaml    png      plantuml.jar
+// xlsx    yaml
+// TODO:
+// dbase   yaml     orginal yaml
 
 func main() {
 
@@ -50,230 +56,269 @@ func main() {
 		}
 	}
 	ofileFlag := func(file *string, usage string) *cli.StringFlag {
-		if xstrings.IsBlank(usage) {
+		if usage == "" {
 			usage = "Output file"
 		}
 		return &cli.StringFlag{
 			Name:        "output",
 			Aliases:     []string{"o"},
 			Usage:       usage,
-			Required:    true,
 			Destination: file,
 		}
 	}
-	validInOutFile := func(ifile string, iexts []string, ofile string, oexts []string) bool {
-		iext := strings.ToLower(filepath.Ext(ifile))
-		oext := strings.ToLower(filepath.Ext(ofile))
-		if xstrings.IsNotBlank(ifile) && xstrings.IsNotBlank(ofile) {
-			return funk.Contains(iexts, iext) && funk.Contains(oexts, oext)
-		}
-		return funk.Contains(iexts, iext)
-	}
+	// validInOutFile := func(ifile string, iexts []string, ofile string, oexts []string) bool {
+	// 	iext := strings.ToLower(filepath.Ext(ifile))
+	// 	oext := strings.ToLower(filepath.Ext(ofile))
+	// 	if ifile != "" && ofile != "" {
+	// 		return lo.Contains(iexts, iext) && lo.Contains(oexts, oext)
+	// 	}
+	// 	return lo.Contains(iexts, iext)
+	// }
 
-	convertCmd := &cli.Command{
-		Name:    "convert",
-		Aliases: []string{"c"},
-		Usage:   "Convert to other format",
-	}
 	cliapp.Commands = append(cliapp.Commands, func() *cli.Command {
-		return convertCmd
-	}())
-
-	convertCmd.Subcommands = append(convertCmd.Subcommands, func() *cli.Command {
 		var ifile, ofile string
 		return &cli.Command{
-			Name:    "yaml",
-			Aliases: []string{"y"},
+			Name:    "transform",
+			Aliases: []string{"t"},
+			Usage:   "Transform to other format",
 			Flags: []cli.Flag{
 				ifileFlag(&ifile),
 				ofileFlag(&ofile, ""),
 			},
 			Action: func(c *cli.Context) error {
-				if validInOutFile(ifile, []string{".xlsx"}, "", []string{}) {
-					tx := xmfr.NewXMFR()
-					if err := tx.LoadXlsx(ifile); err != nil {
-						return eris.Wrapf(err, "failed to load %s", ifile)
-					}
-					if err := tx.SaveToYaml(ofile); err != nil {
-						return eris.Wrapf(err, "failed output to %s", ofile)
-					}
-					return nil
-				}
-				return eris.Errorf("failed to convert %s to %s", ifile, ofile)
-			},
-		}
-	}())
+				iext := strings.ToLower(filepath.Ext(ifile))
+				oext := lo.Ternary(ofile != "", strings.ToLower(filepath.Ext(ofile)), "")
 
-	convertCmd.Subcommands = append(convertCmd.Subcommands, func() *cli.Command {
-		var args xmfr.ExcelArgs
-		// var ifile, ofile string
-		return &cli.Command{
-			Name:    "excel",
-			Aliases: []string{"e"},
-			Flags: []cli.Flag{
-				ifileFlag(&args.InFile),
-				ofileFlag(&args.OutFile, "Output file (.xlsx)"),
-				&cli.BoolFlag{
-					Name:        "simple",
-					Usage:       "simple output, only PK & FK",
-					Value:       false,
-					Required:    false,
-					Destination: &args.Simple,
-				},
-			},
-			Action: func(c *cli.Context) error {
-				if validInOutFile(args.InFile, []string{".yml", ".yaml"}, args.OutFile, []string{".xlsx"}) {
-					tx := xmfr.NewXMFR()
-					tx.LoadYaml(args.InFile)
-					if err := tx.SaveToXlsx(args); err != nil {
-						return eris.Wrapf(err, "failed output to %s", args.OutFile)
+				if iext == ".yml" && (oext == ".yml" || oext == "") {
+					data, err := fyml.ReadYaml(ifile)
+					if err != nil {
+						return tracerr.Wrap(err)
+					}
+					if err := fyml.WriteYaml(data, ofile); err != nil {
+						return tracerr.Wrap(err)
 					}
 					return nil
 				}
-				if validInOutFile(args.InFile, []string{".xlsx"}, args.OutFile, []string{".yml", ".yaml"}) {
-					tx := xmfr.NewXMFR()
-					tx.LoadXlsx(args.InFile)
-					if err := tx.SaveToYaml(args.OutFile); err != nil {
-						return eris.Wrapf(err, "failed output to %s", args.OutFile)
-					}
-					return nil
-				}
-				return eris.Errorf("failed to convert %s to %s", args.InFile, args.OutFile)
-			},
-		}
-	}())
 
-	convertCmd.Subcommands = append(convertCmd.Subcommands, func() *cli.Command {
-		// var ifile, ofile, tfile string
-		var args xmfr.TextArgs
-		return &cli.Command{
-			Name:    "text",
-			Aliases: []string{"t"},
-			Flags: []cli.Flag{
-				ifileFlag(&args.InFile),
-				ofileFlag(&args.OutFile, "Output file ('stdout' output to console)"),
-				&cli.StringFlag{
-					Name:        "template",
-					Aliases:     []string{"t"},
-					Usage:       "Template file",
-					Required:    true,
-					Destination: &args.TemplateFile,
-				},
-				&cli.StringFlag{
-					Name:        "pattern",
-					Aliases:     []string{"p"},
-					Usage:       "Table name pattern, e.g. table*",
-					Value:       "",
-					Required:    false,
-					Destination: &args.Pattern,
-				},
-			},
-			Action: func(c *cli.Context) error {
-				if validInOutFile(args.InFile, []string{".yml", ".yaml"}, "", []string{}) {
-					tx := xmfr.NewXMFR()
-					tx.LoadYaml(args.InFile)
-					if err := tx.SaveToText(args); err != nil {
-						return eris.Wrapf(err, "failed output to %s", args.OutFile)
-					}
+				if iext == ".yml" && oext == ".xlsx" {
 					return nil
 				}
-				return eris.Errorf("failed to convert %s to %s", args.InFile, args.OutFile)
-			},
-		}
-	}())
 
-	convertCmd.Subcommands = append(convertCmd.Subcommands, func() *cli.Command {
-		var args xmfr.DiagramArgs
-		return &cli.Command{
-			Name:    "diagram",
-			Aliases: []string{"d"},
-			Flags: []cli.Flag{
-				ifileFlag(&args.InFile),
-				ofileFlag(&args.OutFile, "Output file (.wsd, .pu, .puml, .plantuml, .iuml)"),
-				&cli.StringFlag{
-					Name:        "type",
-					Aliases:     []string{"t"},
-					Usage:       "Diagram type (ER)",
-					Value:       "ER",
-					Required:    false,
-					Destination: &args.DigType,
-				},
-				&cli.StringFlag{
-					Name:        "schema",
-					Aliases:     []string{"s"},
-					Usage:       "Schema name",
-					Value:       "",
-					Required:    false,
-					Destination: &args.Schema,
-				},
-				&cli.StringFlag{
-					Name:        "pattern",
-					Aliases:     []string{"p"},
-					Usage:       "Table name pattern, e.g. table*",
-					Value:       "",
-					Required:    false,
-					Destination: &args.Pattern,
-				},
-				&cli.StringFlag{
-					Name:        "jar",
-					Aliases:     []string{"j"},
-					Usage:       "plantuml.jar file",
-					Value:       "",
-					Required:    false,
-					Destination: &args.JarFile,
-				},
-				&cli.BoolFlag{
-					Name:        "simple",
-					Usage:       "simple output, only PK & FK",
-					Value:       false,
-					Required:    false,
-					Destination: &args.Simple,
-				},
-				&cli.BoolFlag{
-					Name:        "fk",
-					Usage:       "include foreign key name in the line",
-					Value:       false,
-					Required:    false,
-					Destination: &args.IncludeFK,
-				},
-			},
-			Action: func(c *cli.Context) error {
-				if validInOutFile(args.InFile, []string{".yml", ".yaml"}, args.OutFile, []string{".wsd", ".pu", ".puml", ".plantuml", ".iuml"}) {
-					tx := xmfr.NewXMFR()
-					tx.LoadYaml(args.InFile)
-					if err := tx.SaveToPlantUML(args); err != nil {
-						return eris.Wrapf(err, "failed output to %s", args.OutFile)
-					}
-					if xstrings.IsNotBlank(args.JarFile) {
-						if err := sh.Command("java", "-jar", args.JarFile, args.OutFile).Run(); err != nil {
-							return eris.Wrapf(err, "failed to generate the diagram")
-						}
-					}
-					return nil
-				}
-				return eris.Errorf("failed to convert %s to %s", args.InFile, args.OutFile)
-			},
-		}
-	}())
-
-	cliapp.Commands = append(cliapp.Commands, func() *cli.Command {
-		var ifile string
-		return &cli.Command{
-			Name:    "verify",
-			Aliases: []string{"v"},
-			Usage:   "Verify the foreign key",
-			Flags: []cli.Flag{
-				ifileFlag(&ifile),
-			},
-			Action: func(c *cli.Context) error {
-				tx := xmfr.NewXMFR()
-				tx.LoadYaml(ifile)
-				return tx.Verify()
+				return nil
 			},
 		}
 	}())
 
 	if err := cliapp.Run(os.Args); err != nil {
-		logrus.Error(eris.ToString(err, debug))
+		tracerr.Print(err)
 	}
+
+	// convert command
+	// convertCmd := &cli.Command{
+	// 	Name:    "convert",
+	// 	Aliases: []string{"c"},
+	// 	Usage:   "Convert to other format",
+	// }
+	// cliapp.Commands = append(cliapp.Commands, func() *cli.Command {
+	// 	return convertCmd
+	// }())
+
+	// // from yml to xlsx
+	// convertCmd.Subcommands = append(convertCmd.Subcommands, func() *cli.Command {
+	// 	var ifile, ofile string
+	// 	return &cli.Command{
+	// 		Name:    "yaml",
+	// 		Aliases: []string{"y"},
+	// 		Flags: []cli.Flag{
+	// 			ifileFlag(&ifile),
+	// 			ofileFlag(&ofile, ""),
+	// 		},
+	// 		Action: func(c *cli.Context) error {
+	// 			if validInOutFile(ifile, []string{".xlsx"}, "", []string{}) {
+	// 				tx := xmfr.NewXMFR()
+	// 				if err := tx.LoadXlsx(ifile); err != nil {
+	// 					return eris.Wrapf(err, "failed to load %s", ifile)
+	// 				}
+	// 				if err := tx.SaveToYaml(ofile); err != nil {
+	// 					return eris.Wrapf(err, "failed output to %s", ofile)
+	// 				}
+	// 				return nil
+	// 			}
+	// 			return eris.Errorf("failed to convert %s to %s", ifile, ofile)
+	// 		},
+	// 	}
+	// }())
+
+	// convertCmd.Subcommands = append(convertCmd.Subcommands, func() *cli.Command {
+	// 	var args xmfr.ExcelArgs
+	// 	// var ifile, ofile string
+	// 	return &cli.Command{
+	// 		Name:    "excel",
+	// 		Aliases: []string{"e"},
+	// 		Flags: []cli.Flag{
+	// 			ifileFlag(&args.InFile),
+	// 			ofileFlag(&args.OutFile, "Output file (.xlsx)"),
+	// 			&cli.BoolFlag{
+	// 				Name:        "simple",
+	// 				Usage:       "simple output, only PK & FK",
+	// 				Value:       false,
+	// 				Required:    false,
+	// 				Destination: &args.Simple,
+	// 			},
+	// 		},
+	// 		Action: func(c *cli.Context) error {
+	// 			if validInOutFile(args.InFile, []string{".yml", ".yaml"}, args.OutFile, []string{".xlsx"}) {
+	// 				tx := xmfr.NewXMFR()
+	// 				tx.LoadYaml(args.InFile)
+	// 				if err := tx.SaveToXlsx(args); err != nil {
+	// 					return eris.Wrapf(err, "failed output to %s", args.OutFile)
+	// 				}
+	// 				return nil
+	// 			}
+	// 			if validInOutFile(args.InFile, []string{".xlsx"}, args.OutFile, []string{".yml", ".yaml"}) {
+	// 				tx := xmfr.NewXMFR()
+	// 				tx.LoadXlsx(args.InFile)
+	// 				if err := tx.SaveToYaml(args.OutFile); err != nil {
+	// 					return eris.Wrapf(err, "failed output to %s", args.OutFile)
+	// 				}
+	// 				return nil
+	// 			}
+	// 			return eris.Errorf("failed to convert %s to %s", args.InFile, args.OutFile)
+	// 		},
+	// 	}
+	// }())
+
+	// convertCmd.Subcommands = append(convertCmd.Subcommands, func() *cli.Command {
+	// 	// var ifile, ofile, tfile string
+	// 	var args xmfr.TextArgs
+	// 	return &cli.Command{
+	// 		Name:    "text",
+	// 		Aliases: []string{"t"},
+	// 		Flags: []cli.Flag{
+	// 			ifileFlag(&args.InFile),
+	// 			ofileFlag(&args.OutFile, "Output file ('stdout' output to console)"),
+	// 			&cli.StringFlag{
+	// 				Name:        "template",
+	// 				Aliases:     []string{"t"},
+	// 				Usage:       "Template file",
+	// 				Required:    true,
+	// 				Destination: &args.TemplateFile,
+	// 			},
+	// 			&cli.StringFlag{
+	// 				Name:        "pattern",
+	// 				Aliases:     []string{"p"},
+	// 				Usage:       "Table name pattern, e.g. table*",
+	// 				Value:       "",
+	// 				Required:    false,
+	// 				Destination: &args.Pattern,
+	// 			},
+	// 		},
+	// 		Action: func(c *cli.Context) error {
+	// 			if validInOutFile(args.InFile, []string{".yml", ".yaml"}, "", []string{}) {
+	// 				tx := xmfr.NewXMFR()
+	// 				tx.LoadYaml(args.InFile)
+	// 				if err := tx.SaveToText(args); err != nil {
+	// 					return eris.Wrapf(err, "failed output to %s", args.OutFile)
+	// 				}
+	// 				return nil
+	// 			}
+	// 			return eris.Errorf("failed to convert %s to %s", args.InFile, args.OutFile)
+	// 		},
+	// 	}
+	// }())
+
+	// convertCmd.Subcommands = append(convertCmd.Subcommands, func() *cli.Command {
+	// 	var args xmfr.DiagramArgs
+	// 	return &cli.Command{
+	// 		Name:    "diagram",
+	// 		Aliases: []string{"d"},
+	// 		Flags: []cli.Flag{
+	// 			ifileFlag(&args.InFile),
+	// 			ofileFlag(&args.OutFile, "Output file (.wsd, .pu, .puml, .plantuml, .iuml)"),
+	// 			&cli.StringFlag{
+	// 				Name:        "type",
+	// 				Aliases:     []string{"t"},
+	// 				Usage:       "Diagram type (ER)",
+	// 				Value:       "ER",
+	// 				Required:    false,
+	// 				Destination: &args.DigType,
+	// 			},
+	// 			&cli.StringFlag{
+	// 				Name:        "schema",
+	// 				Aliases:     []string{"s"},
+	// 				Usage:       "Schema name",
+	// 				Value:       "",
+	// 				Required:    false,
+	// 				Destination: &args.Schema,
+	// 			},
+	// 			&cli.StringFlag{
+	// 				Name:        "pattern",
+	// 				Aliases:     []string{"p"},
+	// 				Usage:       "Table name pattern, e.g. table*",
+	// 				Value:       "",
+	// 				Required:    false,
+	// 				Destination: &args.Pattern,
+	// 			},
+	// 			&cli.StringFlag{
+	// 				Name:        "jar",
+	// 				Aliases:     []string{"j"},
+	// 				Usage:       "plantuml.jar file",
+	// 				Value:       "",
+	// 				Required:    false,
+	// 				Destination: &args.JarFile,
+	// 			},
+	// 			&cli.BoolFlag{
+	// 				Name:        "simple",
+	// 				Usage:       "simple output, only PK & FK",
+	// 				Value:       false,
+	// 				Required:    false,
+	// 				Destination: &args.Simple,
+	// 			},
+	// 			&cli.BoolFlag{
+	// 				Name:        "fk",
+	// 				Usage:       "include foreign key name in the line",
+	// 				Value:       false,
+	// 				Required:    false,
+	// 				Destination: &args.IncludeFK,
+	// 			},
+	// 		},
+	// 		Action: func(c *cli.Context) error {
+	// 			if validInOutFile(args.InFile, []string{".yml", ".yaml"}, args.OutFile, []string{".wsd", ".pu", ".puml", ".plantuml", ".iuml"}) {
+	// 				tx := xmfr.NewXMFR()
+	// 				tx.LoadYaml(args.InFile)
+	// 				if err := tx.SaveToPlantUML(args); err != nil {
+	// 					return eris.Wrapf(err, "failed output to %s", args.OutFile)
+	// 				}
+	// 				if args.JarFile != "" {
+	// 					if err := sh.Command("java", "-jar", args.JarFile, args.OutFile).Run(); err != nil {
+	// 						return eris.Wrapf(err, "failed to generate the diagram")
+	// 					}
+	// 				}
+	// 				return nil
+	// 			}
+	// 			return eris.Errorf("failed to convert %s to %s", args.InFile, args.OutFile)
+	// 		},
+	// 	}
+	// }())
+
+	// cliapp.Commands = append(cliapp.Commands, func() *cli.Command {
+	// 	var ifile string
+	// 	return &cli.Command{
+	// 		Name:    "verify",
+	// 		Aliases: []string{"v"},
+	// 		Usage:   "Verify the foreign key",
+	// 		Flags: []cli.Flag{
+	// 			ifileFlag(&ifile),
+	// 		},
+	// 		Action: func(c *cli.Context) error {
+	// 			tx := xmfr.NewXMFR()
+	// 			tx.LoadYaml(ifile)
+	// 			return tx.Verify()
+	// 		},
+	// 	}
+	// }())
+
+	// if err := cliapp.Run(os.Args); err != nil {
+	// 	logrus.Error(eris.ToString(err, debug))
+	// }
 }
