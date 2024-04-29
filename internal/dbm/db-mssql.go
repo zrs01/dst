56f05d518dde83d527930e6d486ef96888dceef6
Rae Wong <rae.wong@gmail.com>
2024-04-29 10:46:13 +0800

    update

--- internal/dbm/db-mssql.go
+++ internal/dbm/db-mssql.go
@@ -1,7 +1,9 @@
 package dbm
 
 import (
+	"database/sql"
 	"fmt"
+	"strings"
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/go-sqlx/sqlx"
@@ -72,24 +74,61 @@ func (s *MssqlService) buildSchemas(dSchemas *[]MsSqlSchema, dTables *[]MsSqlTab
 func (s *MssqlService) buildTable(dSchema MsSqlSchema, dTables *[]MsSqlTable, dColumns *[]MsSqlColumn, dKeyColumnUsages *[]MsSqlKeyColumnUsage) (*[]model.Table, error) {
 	mTables := make([]model.Table, len(*dTables))
 	for index, dTable := range *dTables {
-		// tableColumns := lo.Filter(*dColumns, func(dColumn MsSqlColumn, _ int) bool {
-		// 	return *dColumn.TableSchema == *dSchema.SchemaName && *dColumn.TableName == *dTable.TableName
-		// })
-		// mColumns, err := s.buildColumn(dTable, &tableColumns, dKeyColumnUsages)
-		// if err != nil {
-		// 	return nil, tracerr.Wrap(err)
-		// }
+		tableColumns := lo.Filter(*dColumns, func(dColumn MsSqlColumn, _ int) bool {
+			return *dColumn.TableSchema == *dSchema.SchemaName && *dColumn.TableName == *dTable.TableName
+		})
+		mColumns, err := s.buildColumn(dTable, &tableColumns, dKeyColumnUsages)
+		if err != nil {
+			return nil, tracerr.Wrap(err)
+		}
 		mTables[index] = model.Table{
-			Name: *dTable.TableName,
-			// Columns: *mColumns,
+			Name:    *dTable.TableName,
+			Columns: *mColumns,
 		}
 	}
 	return &mTables, nil
 }
 
+func (s *MssqlService) buildColumn(dTable MsSqlTable, dColumns *[]MsSqlColumn, dKeyColumnUsages *[]MsSqlKeyColumnUsage) (*[]model.Column, error) {
+	mColumns := make([]model.Column, len(*dColumns))
+	for index, dColumn := range *dColumns {
+		mColumn := model.Column{
+			Name:     *dColumn.ColumnName,
+			DataType: *dColumn.DataType,
+		}
+
+		if *dColumn.IsNullable != "YES" {
+			mColumn.NotNull = "Y"
+		}
+
+		if _, ok := lo.Find(*dKeyColumnUsages, func(dKeyColumnUsage MsSqlKeyColumnUsage) bool {
+			return *dKeyColumnUsage.ConstraintName == "PRIMARY" && *dKeyColumnUsage.ColumnName == *dColumn.ColumnName
+		}); ok {
+			mColumn.Identity = "Y"
+		}
+
+		// if usage, ok := lo.Find(*dKeyColumnUsages, func(dKeyColumnUsage MsSqlKeyColumnUsage) bool {
+		// 	return *dKeyColumnUsage.ConstraintName != "PRIMARY" && *dKeyColumnUsage.ColumnName == *dColumn.ColumnName
+		// }); ok {
+		// 	mColumn.ForeignKey = fmt.Sprintf("%s.%s", *usage.ReferencedTableName, *usage.ReferencedColumnName)
+		// }
+
+		if strings.Contains(strings.ToLower(*dColumn.DataType), "char") {
+			mColumn.DataType = fmt.Sprintf("%s(%d)", *dColumn.DataType, *dColumn.CharacterMaximumLength)
+		} else if strings.Contains(strings.ToLower(*dColumn.DataType), "decimal") {
+			if dColumn.NumericScale != nil && *dColumn.NumericScale > 0 {
+				mColumn.DataType = fmt.Sprintf("%s(%d,%d)", *dColumn.DataType, *dColumn.NumericPrecision, *dColumn.NumericScale)
+			}
+		}
+		mColumns[index] = mColumn
+	}
+	return &mColumns, nil
+}
+
 func (s *MssqlService) getSchemas(db *sqlx.DB, schemaName string) (*[]MsSqlSchema, error) {
 	dSchemas := []MsSqlSchema{}
-	if err := db.Select(&dSchemas, "select * from INFORMATION_SCHEMA.SCHEMATA where SCHEMA_NAME = @p1;", schemaName); err != nil {
+	if err := db.Select(&dSchemas, "select * from INFORMATION_SCHEMA.SCHEMATA where SCHEMA_NAME = @SCHEMA",
+		sql.Named("SCHEMA", schemaName)); err != nil {
 		return nil, tracerr.Wrap(err)
 	}
 	return &dSchemas, nil
@@ -99,8 +138,8 @@ func (s *MssqlService) getTables(db *sqlx.DB, schemas *[]MsSqlSchema, dbName str
 	dTables := []MsSqlTable{}
 	dSchemaNames := lo.Map(*schemas, func(dSchema MsSqlSchema, _ int) string { return *dSchema.SchemaName })
 	query, args, err := sqlx.In(`
-		select * from INFORMATION_SCHEMA.TABLES where TABLE_CATALOG = @p1 and TABLE_SCHEMA in (@p2) order by TABLE_NAME`,
-		dbName, dSchemaNames)
+		select * from INFORMATION_SCHEMA.TABLES where TABLE_TYPE = 'BASE TABLE' and TABLE_CATALOG = @CAT and TABLE_SCHEMA in (@SCHEMAS) order by TABLE_NAME`,
+		sql.Named("CAT", dbName), sql.Named("SCHEMAS", strings.Join(dSchemaNames, ",")))
 	if err != nil {
 		return nil, tracerr.Wrap(err)
 	}
@@ -108,7 +147,6 @@ func (s *MssqlService) getTables(db *sqlx.DB, schemas *[]MsSqlSchema, dbName str
 	if err := db.Select(&dTables, query, args...); err != nil {
 		return nil, tracerr.Wrap(err)
 	}
-	spew.Dump(dSchemaNames)
 	return &dTables, nil
 }
 
@@ -117,8 +155,8 @@ func (s *MssqlService) getColumns(db *sqlx.DB, schemas *[]MsSqlSchema, tables *[
 	dSchemaNames := lo.Map(*schemas, func(dSchema MsSqlSchema, _ int) string { return *dSchema.SchemaName })
 	dTableNames := lo.Map(*tables, func(dTable MsSqlTable, _ int) string { return *dTable.TableName })
 	query, args, err := sqlx.In(
-		`select * from INFORMATION_SCHEMA.COLUMNS where TABLE_SCHEMA in (?) and TABLE_NAME in (?) order by TABLE_SCHEMA, TABLE_NAME, ORDINAL_POSITION`,
-		dSchemaNames, dTableNames)
+		`select * from INFORMATION_SCHEMA.COLUMNS where TABLE_SCHEMA in (@SCHEMAS) and TABLE_NAME in (@TABLES) order by TABLE_SCHEMA, TABLE_NAME, ORDINAL_POSITION`,
+		sql.Named("SCHEMAS", strings.Join(dSchemaNames, `,`)), sql.Named("TABLES", strings.Join(dTableNames, `,`)))
 	if err != nil {
 		return nil, tracerr.Wrap(err)
 	}
@@ -126,6 +164,10 @@ func (s *MssqlService) getColumns(db *sqlx.DB, schemas *[]MsSqlSchema, tables *[
 	if err := db.Select(&dColumns, query, args...); err != nil {
 		return nil, tracerr.Wrap(err)
 	}
+	spew.Dump(args)
+	spew.Dump(dSchemaNames)
+	spew.Dump(dTableNames)
+	spew.Dump(dColumns)
 	return &dColumns, nil
 }
 
